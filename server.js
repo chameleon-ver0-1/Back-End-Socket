d27f978e900490473053f3f00371f6be1477fe01
onisley <onisley@naver.com>
2019-10-09 16:51:38 +0900

    [UPDATE] socket 메시지 송,수신 내용 변경

--- server.js
+++ server.js
@@ -3,40 +3,37 @@ io.set('origins', '*:*');
 
 io.sockets.on('connection', socket => {
 
+    // connection 수신되면 Client에 connected 송신
     socket.emit('connection', {
+        color: (Math.round()*0xffffff).toString(16),
         type : 'connected'
     });
 
     socket.on('connection', data => {
 
+        // 새로 들어온 사용자면 해당 사용자에게는 welcome message와 color, 채팅방에는 해당 사용자 연결 메시지 송신
         if(data.type === 'join') {
 
             socket.join(data.room);
-
-            // depracated
-            // socket.set('room', data.room);
             socket.room = data.room;
 
             socket.emit('system', {
-                message : 'welcome to chat room'
+                message: '회의실에 입장하셨습니다.'
             });
 
+            // TODO: 로컬 DB에 데이터 쌓기
             socket.broadcast.to(data.room).emit('system', {
-                message : `${data.name} is connected`
+                message : `${data.name} 님이 회의에 참여하셨습니다.`
             });
         }
-
     });
 
+    // 유저가 보낸 data(color, name, message) 그대로 채팅방에 echo
     socket.on('user', data => {
-
-        // depracated
-        // socket.get('room', (error, room) => {
-        // });
-
         var room = socket.room;
 
-        if(room) {
+        if (room) {
+            // TODO: 로컬 DB에 데이터 쌓기
             socket.broadcast.to(room).emit('message', data);
         }
     });
